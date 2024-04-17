f872b8f4c47779df07407a1f12443477a6ffa986
santhoshmarmeto <santhosh@marmeto.com>
2024-04-18 02:29:29 +0530

    cssadded

--- assets/compoent-slide.js
+++ assets/compoent-slide.js
@@ -1,14 +1,49 @@
-// class CustomSlider extends HTMLElement {
-//   constructor() {
-//     super();
-//     this.element = this.querySelector(".splide");
-//     this.options = JSON.parse(this.element.dataset.sliderSettings);
-//     this.mountSplider();
-//   }
+class CustomSlider extends HTMLElement {
+  constructor() {
+    super();
+    this.element = this.querySelector(".splide");
+    this.options = JSON.parse(this.element.dataset.sliderSettings);
+    this.mountSplide();
+  }
 
-//   mountSplider() {
-//     new Splide(this.element, this.options).mount();
-//   }
-// }
+  mountSplide() {
+    const splide = new Splide(this.element, this.options).mount();
+    const desktopimage = Array.from(document.querySelectorAll(".desktopimgaes"));
+    desktopimage.forEach(each => {
+      if(desktopimage.indexOf(each) !== 0){
+        each.classList.add('d-none');
+      }
+    });
+    const slides = document.querySelectorAll('.splide__slide');
+  slides.forEach(each => {
+    if(each.classList.contains('is-active')){
+      each.classList.add('active-slide');
+    }else{
+      each.classList.remove('active-slide');
+    }
+    each.removeAttribute('style');
+  })
+    splide.on('moved', function(index){
+      //changing the image
+      desktopimage.forEach(each => {
+        if(index === desktopimage.indexOf(each)){
+              each.classList.remove('d-none');
+            }else{
+              each.classList.add('d-none');
+          }
+      });
+      //changing the color
+      slides.forEach(each => {
+        if(each.classList.contains('is-active')){
+          each.classList.add('active-slide');
+        }else{
+          each.classList.remove('active-slide');
+        }
+      }) 
+  })
+  
+  
+  }
+}
 
-// customElements.define("author-component", CustomSlider);
+customElements.define("splider-component", CustomSlider);
--- assets/custom-slideshow.js
+++ assets/custom-slideshow.js
@@ -1,14 +1,14 @@
-class CustomSlider extends HTMLElement {
-  constructor() {
-    super();
-    this.element = this.querySelector(".splide");
-    this.options = JSON.parse(this.element.dataset.sliderSettings);
-    this.mountSplider();
-  }
+// class CustomSlider extends HTMLElement {
+//   constructor() {
+//     super();
+//     this.element = this.querySelector(".splide");
+//     this.options = JSON.parse(this.element.dataset.sliderSettings);
+//     this.mountSplider();
+//   }
 
-  mountSplider() {
-    new Splide(this.element, this.options).mount();
-  }
-}
+//   mountSplider() {
+//     new Splide(this.element).mount();
+//   }
+// }
 
-customElements.define("splider-component", CustomSlider);
+// customElements.define("splider-component", CustomSlider);
