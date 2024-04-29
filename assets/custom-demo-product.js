13f97a2d33b7cf24a323a96bc830ad5c52a8bbf5
santhoshmarmeto <santhosh@marmeto.com>
2024-04-29 22:52:47 +0530

    changeproduct

---
+++ assets/custom-demo-product.js
@@ -0,0 +1,29 @@
+class VeenaProductCard extends HTMLElement {
+    constructor() {
+      super();
+  
+      console.log("Product card", this);
+  
+      this.addEventListener("change", (event) => {
+        if (event.target.name == "id") this.variantChange();
+      });
+    }
+  
+    variantChange(){
+      this.selectedVariantId = this.querySelector('[name="id"]').value;
+      let variantUrl = `/products/${this.dataset.handle}?view=product-card&variant=${this.selectedVariantId}`;
+       
+      //Fetching for new html
+      fetch(variantUrl)
+        .then((response) => response.text())
+        .then((responseText) => {
+          const html = new DOMParser().parseFromString(responseText, 'text/html');
+          const responseCard = html.querySelector('veena-product-card');
+          console.log(responseCard);
+         //Change the DOM Elements
+          this.innerHTML = responseCard.innerHTML;
+        });
+    }
+    
+  }
+  customElements.define("veena-product-card", VeenaProductCard);
