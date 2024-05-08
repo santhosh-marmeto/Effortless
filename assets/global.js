6cfe6492874cdd054b81a3a66d8ab291108c903a
santhoshmarmeto <santhosh@marmeto.com>
2024-05-08 23:44:41 +0530

    footeradded

--- assets/global.js
+++ assets/global.js
@@ -1899,3 +1899,52 @@ class PincodeChecker extends HTMLElement {
 }
 
 customElements.define('pincode-checker', PincodeChecker);
+
+
+
+
+/*-------------------------------------custom-atc-------------------------------------------------------*/
+if (document.getElementById("custom-my-btn")) {
+  const btn = document.getElementById("custom-my-btn");
+  const firstNameInput = document.getElementById("firstNameInput");
+
+  const cartQnty = document
+    .querySelector(".quantity__input")
+    .getAttribute("data-cart-quantity");
+  console.log(cartQnty);
+
+  btn.addEventListener("click", () => {
+    const cart = document.querySelector("cart-drawer");
+    const userFirstName = firstNameInput.value;
+
+    let formData = {
+      items: [
+        {
+          id: btn.dataset.productId,
+          quantity: btn.dataset.quantity,
+          properties: {
+            "First name": userFirstName,
+            "_Last Name": "Murugan",
+          },
+          sections: cart.getSectionsToRender().map((section) => section.id),
+        },
+      ],
+    };
+
+    fetch("/cart/add.js", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        console.log(response);
+        cart.open();
+        return response.json();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+}
